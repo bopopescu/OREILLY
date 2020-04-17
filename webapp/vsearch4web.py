cdac7f1158212bf2a949ad1e61704f91ffcbf315
ZMichael <zaplatinme@gmail.com>
2020-04-17 23:53:03 +0700

    Added async processing log

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, copy_current_request_context
 from vsearch import search4letters
 from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+from threading import Thread
 
 
 app = Flask(__name__)
@@ -26,27 +27,39 @@ def do_logout() -> str:
     return 'You are now logged out'
 
 
-def log_request(req: 'flask_request', res: str) -> None:  # Теперь с базой данных
-    with UseDatabase(app.config['dbconfig']) as cursor:
-        _SQL = """insert into log
-                  (phrase, letters, ip, browser_string, results)
-                  values
-                  (%s, %s, %s, %s, %s)"""
-        cursor.execute(_SQL, (req.form['phrase'],
-                              req.form['letters'],
-                              req.remote_addr,
-                              req.user_agent.browser,
-                              res,))
-
-
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
+
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:  # Теперь с базой данных
+        try:
+            with UseDatabase(app.config['dbconfig']) as cursor:
+                _SQL = """insert into log
+                          (phrase, letters, ip, browser_string, results)
+                          values
+                          (%s, %s, %s, %s, %s)"""
+                cursor.execute(_SQL, (req.form['phrase'],
+                                      req.form['letters'],
+                                      req.remote_addr,
+                                      req.user_agent.browser,
+                                      res,))
+        except ConnectionError as err:
+            print('Is your database switched on& Error: ', str(err))
+        except CredentialsError as err:
+            print('User-id/Password issues. Error: ', str(err))
+        except SQLError as err:
+            print('Is your query correct? Error: ', str(err))
+        except Exception as err:
+            print('Something went wrong: ', str(err))
+        return 'Error'
+
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
     try:
-        log_request(request, results)
+        t = Thread(target=log_request, args=(request, results))
+        t.start()
     except Exception as err:
         print('Logging failed with the error: ', str(err))
     return render_template('results.html',
